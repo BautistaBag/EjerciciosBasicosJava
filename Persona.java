a2397396e3c20b608cc4f2bf7cbe3cea049b2c5f
Bautista Bagnarol <81656547+BautistaBag@users.noreply.github.com>
2021-11-15 12:00:15 -0300

    Create Persona.java

---
+++ Persona.java
@@ -0,0 +1,25 @@
+package domain;
+
+//Clase para testear el forEach
+public class Persona {
+
+	private String nombre;
+	
+	public Persona(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Persona{"+ "nombre= "+ nombre + "}";
+	}
+	
+}
