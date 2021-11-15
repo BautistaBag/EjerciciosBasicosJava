4f9d2b99c989cd8493784963b283466d94084568
Bautista Bagnarol <81656547+BautistaBag@users.noreply.github.com>
2021-11-15 11:23:05 -0300

    Create TestEnumeraciones2.java

---
+++ TestEnumeraciones2.java
@@ -0,0 +1,10 @@
+
+public class TestEnumeraciones2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Continente no. 4: "+ Continentes.AMERICA);
+		System.out.println("No. Paises en el 4to continente: "+ Continentes.AMERICA.getPaises());
+		
+}
+}
